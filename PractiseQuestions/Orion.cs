3cc96178edd0c6e87becc70e8514ba292478b72d
devthet <devthet@hotmail.com>
2022-07-02 14:39:03 +0630

    Initial

---
+++ PractiseQuestions/Orion.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PractiseQuestions
+{
+    public class Orion
+    {
+        public static void Main(string[] args){
+            Console.WriteLine("Hello vscode.dev");
+        }
+    }
+}
