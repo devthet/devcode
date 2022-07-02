08afdc57c0d8b4440842f15dd2551b898bb359d5
Ko Thet <54733830+devthet@users.noreply.github.com>
2022-07-02 14:58:41 +0630

    orion-1

--- PractiseQuestions/Orion.cs
+++ PractiseQuestions/Orion.cs
@@ -8,7 +8,13 @@ namespace PractiseQuestions
     public class Orion
     {
         public static void Main(string[] args){
-            Console.WriteLine("Hello vscode.dev");
+            int[] a = {1, 2, 19, 4, 5};
+
+            Console.WriteLine("Output:{0}",isOnionArray(a));
+        }
+
+        private static int isOnionArray(int[] a){
+            return 0;
         }
     }
 }
